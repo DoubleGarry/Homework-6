2cc1e6e21b2a27a863703b6cbb064718b62e3692
semenikhin <garry@nextmail.ru>
2023-01-09 15:04:58 +0300

    hw-6 task1-4

--- src/Main.java
+++ src/Main.java
@@ -11,18 +11,27 @@ public class Main {
     }
     public static void task1(){
         System.out.println("Задача 1");
-
+        for (int q=1; q <=10; q=q+1 ){
+            System.out.println(q);
+        }
     }
     public static void task2(){
         System.out.println("Задача 2");
-
+        for (int i=10; i >=1; i=i-1 ){
+            System.out.println(i);
+        }
     }
     public static void task3(){
         System.out.println("Задача 3");
-
+        for (int w=0; w <=17; w=w+2 ){
+            System.out.println(w);
+        }
     }
     public static void task4(){
         System.out.println("Задача 4");
+        for (int e=10; e >=-10; e=e-1 ){
+            System.out.println(e);
+        }
 
     }
     public static void task5(){
